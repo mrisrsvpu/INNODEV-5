dd80280c1601805bed881259b6c6bcdfd2a59c27
Alex <33393952+Alexandr18@users.noreply.github.com>
2019-03-18 20:13:44 +0500

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var output_day_date = [];
 var output_disciplina_time = [];
 var day_num = 0;
 var output_disciplina = [];
-
+var name_ped_out = [];
 
 app.get("/group/:id", function(req, res) {
   results_name_group = [];
@@ -18,6 +18,11 @@ app.get("/group/:id", function(req, res) {
   day_num = 0;
   output_disciplina = [];
   output_all_inf = [];
+  name_ped = [];
+  name_ped_out = [];
+  var disc = [];
+  var output_disc = [];
+  var all = [];
   url =
     "http://www.rsvpu.ru/raspisanie-zanyatij-ochnoe-otdelenie/?v_gru=" +
     req.params.id;
@@ -28,22 +33,59 @@ app.get("/group/:id", function(req, res) {
     var $ = cheerio.load(res.body);
     results_name_group = $(".rasp_header").text(); //ок
 
-    $(".disciplina_info").each((i, elem) => {
-      var $a = $(elem).find("p");
-      var disciplina = {
-        title: $a.text().split("  ")
-      };
-      output_disciplina.push(disciplina);
-    });
+    // $(".disciplina_cont").each((i, elem) => {
+    //   var $a = $(elem);
+    //   var b = $a.find(".disciplina_time").text();
+    //   var $a = $(elem);
+    //   // var c =
+    //   $a.find(".disciplina_info").each((i, elem) => {
+    //     var $c = $(elem);
+    //     var disc = {
+    //       name: $c.text()
+    //     };
+    //     output_disc.push(disc);
+    //   });
+    //   var disciplina = {
+    //     name: output_disc
+    //   };
+
+    //   all.push(disciplina);
+    // });
+    //console.log(all);
+    $(".disciplina_info")
+      .children()
+      .each((i, elem) => {
+        var $a = $(elem);
+        var name_ped = {
+          time: 1,
+          title: $a.text().split("  ");
+        };
+        name_ped_out.push(name_ped);
+      });
+    console.log(name_ped_out);
+    // $(".disciplina_info")
+    //   .children()
+    //   .each((i, elem) => {
+    //     var $a = $(elem);
+    //     $a = $a.text().split("  ");
+    //     var name_ped = {
+    //       time: 21,
+    //       title: $a
+    //     };
+    //     name_ped_out.push(name_ped);
+    //   });
+
+    //console.log(name_ped_out);
 
     $(".disciplina_time").each((i, elem) => {
       var $a = $(elem).find("p");
       var disciplina_time = {
         time: $a.text().split(" ")
       };
-
+      // console.log(output_disciplina);
       output_disciplina_time.push(disciplina_time);
     });
+
     $(".day_date").each((i, elem) => {
       var $a = $(elem).find("p");
       var day_date = {
@@ -51,8 +93,9 @@ app.get("/group/:id", function(req, res) {
       };
       output_day_date.push(day_date);
     });
+    //console.log(output_day_date);
 
-    for (var i = 0; i < output_disciplina.length; i++) {
+    for (var i = 0; i < name_ped_out.length; i++) {
       day_num =
         i < 14
           ? 0
@@ -98,14 +141,14 @@ app.get("/group/:id", function(req, res) {
           : 1;
 
       var all_inf = {
-        name_of_group: results_name_group, //Номер группы
-        data: output_day_date[day_num].data[day], // Дата
-        number: output_disciplina_time[i].time[0], // Номер пары
-        time: output_disciplina_time[i].time[1], // Время начала пары
-        name: output_disciplina[i].title[0], //название пары
-        type: output_disciplina[i].title[1], //Тип пары
-        class_room: output_disciplina[i].title[2], //Кабинет
-        name_of_pedagog: output_disciplina[i].title[3] //Имя преподавателя
+        // name_of_group: results_name_group, //Номер группы
+        // data: output_day_date[day_num].data[day], // Дата
+        // number: output_disciplina_time[i], //.time[0], // Номер пары
+        // //time: output_disciplina_time[i].time[1], // Время начала пары
+        // name: name_ped_out[i].title[0], //название пары
+        // type: name_ped_out[i].title[1], //Тип пары
+        // class_room: name_ped_out[i].title[2], //Кабинет
+        // name_of_pedagog: name_ped_out[i].title[3] //Имя преподавателя
       };
       output_all_inf.push(all_inf);
     }
